cf8ecfc398b07a279a6dd2152c16a9a7b6d061b0
Mahesh Vashisth <Maheshvashisth1111@gmail.com>
2024-04-29 22:31:03 +0530

    updated index.js

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ app.use(express.json());
 
 app.use("/admin", adminRouter);
 app.use("/user", userRouter);
+app.get("/", (req, res) => res.json({ msg: "server is running" }));
 
 const dbUrl = process.env.DB_URL;
 
